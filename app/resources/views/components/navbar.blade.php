dd89ff9d3a2ebf0702f125b985e4a5814b5f3f17
Aki <103581174+Akii-Dev@users.noreply.github.com>
2025-11-13 14:20:34 +0100

    fix href links in navbar and register

--- app/resources/views/components/navbar.blade.php
+++ app/resources/views/components/navbar.blade.php
@@ -18,7 +18,7 @@
 
         <hr class="w-[80%] my-8 bg-gray-400 border-1">
         <a href="#" class="my-6 underline decoration-black yellow1text">Booken</a>
-        <a href="#" class="my-6 underline decoration-black yellow1text">Account</a>
+        <a href="{{route('user.login')}}" class="my-6 underline decoration-black yellow1text">Account</a>
         {{-- change ./rules to {{route('rules')}} when adding a rules view route in a controller--}}
         <a href="./rules" class="my-6 underline decoration-black yellow1text">Huisregels</a> 
     </div>
