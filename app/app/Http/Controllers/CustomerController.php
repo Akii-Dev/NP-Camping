2ea6c38848f1bea4211adc5f54381383f30fc060
Aki <103581174+Akii-Dev@users.noreply.github.com>
2025-11-18 10:08:25 +0100

    add routing for user management

--- app/app/Http/Controllers/CustomerController.php
+++ app/app/Http/Controllers/CustomerController.php
@@ -8,17 +8,30 @@ use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
-    // resource methods would go here. we'll work with edit user account. showing the view page and logic
-    public function edit(Request $request, $id) 
+
+    public function show(Request $request, $id)
     {
+        $customer = Customer::find($id);
+        return view('customer.show', compact('customer'));
+    }
+    // resource methods would go here. we'll work with edit user account. showing the view page and logic
+    public function edit(Request $request, $id)
+    {
+
+        if (!session()->has('user')) {
+            return redirect('/login')->with('error', 'Je moet ingelogd zijn om je profiel te bewerken');
+        }
+
+        if (session('user.id') != $id) {
+            return redirect('/login')->with('error', 'Je kunt alleen je eigen profiel bewerken');
+        }
 
         $customer = Customer::find($id);
 
-        var_dump(session('user.id')); 
-        var_dump(session('user.email')); 
-        var_dump($customer->user_id); 
+        var_dump(session('user.id'));
+        var_dump(session('user.email'));
+        var_dump($customer->user_id);
         var_dump($customer->user->email); // this works because of the relationship defined in the Customer model
-        // return view('customers.edit', compact('customer')); // compact is the same as ['customer' => $customer]
+        return view('customers.edit', compact('customer')); // compact is the same as ['customer' => $customer]
     }
-
 }
