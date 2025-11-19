0ca9806aa6e2dc9b0bbd80e7a952c2b2d27ce5fc
Luuk Jongeneel <ljongeneel@st.noorderpoort.nl>
2025-11-19 14:14:28 +0100

    navbar werkt op korte pc paginas

--- app/resources/views/components/navbar.blade.php
+++ app/resources/views/components/navbar.blade.php
@@ -15,7 +15,7 @@
         <a href="#" class="my-6 underline decoration-black yellow1text">Home</a>
         <a href="#" class="my-6 underline decoration-black yellow1text">Booken</a>
         <a href="{{route('user.login')}}" class="my-6 underline decoration-black yellow1text">Account</a>
-        {{-- change ./rules to {{route('rules')}} when adding a rules view route in a controller--}}
+        {{-- change ./rules to {{route('rules')}} when adding a rules view route in a controller --}}
         <a href="./rules" class="my-6 underline decoration-black yellow1text">Huisregels</a> 
     </div>
 </nav>
