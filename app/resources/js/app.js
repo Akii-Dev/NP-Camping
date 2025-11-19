23caaf07dda243285e889fd3cc536db6904a1437
Niels ten Hove <ntenhove@st.noorderpoort.nl>
2025-11-19 09:03:09 +0100

    merge conflict diagnosis

--- app/resources/js/app.js
+++ app/resources/js/app.js
@@ -1,40 +1,55 @@
 import './bootstrap';
 
+// ScreenSize
+
+function handleResize() {
+    const w = window.innerWidth;
+    return w;
+}
+
+window.addEventListener('resize', handleResize);
+
+handleResize();
+
 // navbar side + fade
 
 const navbar = document.getElementById('sideNav');
 const navContent = document.getElementById('navContent');
-const fadePoint = window.innerHeight * 0.4;
 
-window.addEventListener('scroll', () => {
-    const scrollY = window.scrollY;
+if (handleResize() >= 768) {
+    const fadePoint = window.innerHeight * 0.4;
 
-    if (scrollY > fadePoint) {
-        navbar.classList.add('backdrop-blur-lg', 'w-40');
-        navContent.classList.add('opacity-100');
-        navContent.classList.remove('pointer-events-none');
-    } else {
-        navbar.classList.remove('backdrop-blur-lg', 'w-40');
-        navContent.classList.remove('opacity-100');
-        navContent.classList.add('pointer-events-none');
-    }
-});
+    window.addEventListener('scroll', () => {
+        const scrollY = window.scrollY;
 
-const burgerBtn = document.getElementById('burgerBtn');
-const sideNav = document.getElementById('sideNav');
+        if (scrollY > fadePoint) {
+            navbar.classList.add('backdrop-blur-lg', 'w-40');
+            navContent.classList.add('opacity-100');
+            navContent.classList.remove('pointer-events-none');
+        } else {
+            navbar.classList.remove('backdrop-blur-lg', 'w-40');
+            navContent.classList.remove('opacity-100');
+            navContent.classList.add('pointer-events-none');
+        }
+    });
+} else {
+    const burgerBtn = document.getElementById('burgerBtn');
+    const overlay = document.getElementById('overlay');
+    burgerBtn.addEventListener('click', () => {
+        burgerBtn.classList.add('hidden');
+        navbar.classList.remove('hidden');
+        navbar.classList.add('w-40');
+        navContent.classList.remove('opacity-0', 'pointer-events-none')
+        overlay.classList.remove('hidden');
+        overlay.classList.add('w-40');
+    });
 
-burgerBtn.addEventListener('click', () => {
-    sideNav.classList.toggle('-translate-x-full');
-});
-
-const overlay = document.getElementById('overlay');
-
-burgerBtn.addEventListener('click', () => {
-    sideNav.classList.toggle('-translate-x-full');
-    overlay.classList.toggle('hidden');
-});
-
-overlay.addEventListener('click', () => {
-    sideNav.classList.add('-translate-x-full');
-    overlay.classList.add('hidden');
-});
+    overlay.addEventListener('click', () => {
+        burgerBtn.classList.remove('hidden');
+        navbar.classList.add('hidden');
+        navbar.classList.remove('w-40');
+        navContent.classList.add('opacity-0', 'pointer-events-none')
+        overlay.classList.add('hidden');
+        overlay.classList.remove('w-40');
+    });
+}
