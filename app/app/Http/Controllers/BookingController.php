6de9d751627c45e4628d3d8ab40d6c0e86c2b31d
Aki <103581174+Akii-Dev@users.noreply.github.com>
2025-11-19 14:24:15 +0100

    add routing to menu.create from booking.index

--- app/app/Http/Controllers/BookingController.php
+++ app/app/Http/Controllers/BookingController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Booking;
 use App\Models\Spot;
+use App\Models\Customer;
+use App\Models\User;
 
 class BookingController extends Controller
 {
@@ -13,5 +15,11 @@ class BookingController extends Controller
         // get all spots from the database
         $spots = Spot::all();
         return view('booking.index', compact('spots')); // short for ['spot' => $spot]
+    }
+
+    public function create()
+    {
+        // get user data
+        return view('booking.create');
     }
 }
--- app/resources/views/booking/index.blade.php
+++ app/resources/views/booking/index.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,34 +8,36 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>Document</title>
 </head>
+
 <body>
     @foreach ($spots as $spot)
         @if ($spot->booked == 1)
             <div class="bg-yellow-500 w-32">
-        @elseif ($spot->booked == 0)
-            <div class="bg-green-500 w-28"> {{-- Different width to show difference --}}
-                <button onclick="selectSpot({{ $spot->id }})">Click me</button>
-
+            @elseif ($spot->booked == 0)
+                <div class="bg-green-500 w-28"> {{-- Different width to show difference --}}
+                    <button onclick="selectSpot({{ $spot->id }})">Click me</button>
         @endif
 
-            <h2>Spot {{ $spot->id }}</h2>
-            {{-- <h2>Is it booked? {{ $spot->booked }}</h2> --}}
+        <h2>Spot {{ $spot->id }}</h2>
+        {{-- <h2>Is it booked? {{ $spot->booked }}</h2> --}}
         </div>
     @endforeach
 
-    <div id="popup" class="hidden">
+    <div id="popup" class="hidden absolute top-96 left-96">
         <p id="spot-name"></p>
         <p id="description"></p>
+        <a href="{{ route('booking.create') }}">Boek nu!</a>
     </div>
     {{-- move to other file soontm --}}
     <script>
         function selectSpot(id) {
-            alert("You selected a spot!");
             // Show popup with spot details
             const popup = document.getElementById('popup');
             popup.classList.remove('hidden');
             document.getElementById('spot-name').innerText = id;
+            document.getElementById('description').innerText = "Wat leuk dat je deze plek hebt gekozen!";
         }
     </script>
 </body>
+
 </html>
