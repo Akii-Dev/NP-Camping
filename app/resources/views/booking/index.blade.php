77d63eccdae889b98d72088e466911e57326c20c
Aki <103581174+Akii-Dev@users.noreply.github.com>
2025-11-19 12:52:22 +0100

    add booking spots migration and model

---
+++ app/resources/views/booking/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <title>Document</title>
+</head>
+<body>
+    @for ($i = 0; $i < 10; $i++)
+        <p>Booking Index Page - Item {{ $i  }}</p>
+    @endfor
+</body>
+</html>
