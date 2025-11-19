8c412b607a97bd11544e78383be30f72f503d29d
Aki <103581174+Akii-Dev@users.noreply.github.com>
2025-11-19 13:29:17 +0100

    add popup when attempting to select spot

--- app/resources/views/booking/index.blade.php
+++ app/resources/views/booking/index.blade.php
@@ -13,11 +13,28 @@
             <div class="bg-yellow-500 w-32">
         @elseif ($spot->booked == 0)
             <div class="bg-green-500 w-28"> {{-- Different width to show difference --}}
+                <button onclick="selectSpot({{ $spot->id }})">Click me</button>
+
         @endif
 
             <h2>Spot {{ $spot->id }}</h2>
             {{-- <h2>Is it booked? {{ $spot->booked }}</h2> --}}
         </div>
     @endforeach
+
+    <div id="popup" class="hidden">
+        <p id="spot-name"></p>
+        <p id="description"></p>
+    </div>
+    {{-- move to other file soontm --}}
+    <script>
+        function selectSpot(id) {
+            alert("You selected a spot!");
+            // Show popup with spot details
+            const popup = document.getElementById('popup');
+            popup.classList.remove('hidden');
+            document.getElementById('spot-name').innerText = id;
+        }
+    </script>
 </body>
 </html>
