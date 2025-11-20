6b6ce8b8a8a4bed31a947fc70da25d5544c446fd
Niels <ntenhove@st.noorderpoort.nl>
2025-11-20 11:11:21 +0100

    footer text edit

--- app/resources/views/register.blade.php
+++ app/resources/views/register.blade.php
@@ -37,22 +37,26 @@
                 </div>
             </div>
     </div>
-    <footer class="text-white bg-zinc-900 h-auto w-auto px-2 lg:px-40 py-2 grid grid-row-3 lg:grid-cols-3 lg:h-36 lg:divide-x-1 divide-solid divide-white">
+
+    <footer
+        class="text-white bg-zinc-900 h-auto w-auto px-2 lg:px-40 py-2 grid grid-row-3 lg:grid-cols-3 lg:h-36 lg:divide-x-1 divide-solid divide-white">
         <div class="px-2 text-sm row-span-1 lg:row-span-0 border-b-2 lg:border-b-0">
             <h3 class="font-semibold text-lg">Contact</h3>
             <p>Email: info@npcamping.nl</p>
-            <p>Phone: 06 12312312</p>
+            <p>Phone: 06 12341234</p>
         </div>
         <div class="px-2 text-sm row-span-1 lg:row-span-0 border-b-2 lg:border-b-0">
             <h3 class="font-semibold text-lg">NP Camping</h3>
-            <p>Adres: Borgerweg 23</p>
-            <p>Postcode: 9462RA Gasselte</p>
+            <p>Adres: Kruimelweg 23</p>
+            <p>Postcode: 8462RA Broden</p>
         </div>
         <div class="px-2 text-sm row-span-1 lg:row-span-0 border-b-2 lg:border-b-0">
             <h3 class="font-semibold text-lg">Informatie</h3>
-            <p>Open van 1 april tot en met 31 oktober</p>
+            <p>Wij zijn open van 4 juli tot 16 augustus</p>
+            <p>Dus de hele zomervakantie kunt u boeken</p>
         </div>
     </footer>
+
     {{-- sessions error handles manualy declared errors in the session --}}
     @if (session('error'))
         <div class="bg-red-500">
